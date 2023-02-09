d9533f6fa406948592bf24f954ca0c5ffea3f118
i569071 <ricky.hu@sap.com>
2023-02-09 21:28:59 +0800

    update sql script

--- src/main/java/com/example/demo/entity/User.java
+++ src/main/java/com/example/demo/entity/User.java
@@ -1,8 +1,10 @@
 package com.example.demo.entity;
 
+import com.baomidou.mybatisplus.annotation.TableName;
 import lombok.Data;
 
 @Data
+@TableName("`user`")
 public class User {
     private Long id;
     private String name;
